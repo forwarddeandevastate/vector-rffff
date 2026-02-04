f0d8660ce693065619eb9a3693c63bd7eb1fc20a
Max <censkyv@gmail.com>
2026-02-04 16:26:28 +0300

    update metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const manrope = Manrope({
@@ -22,32 +21,30 @@ export default function RootLayout({
     <html lang="ru">
       <head>
         {/* Yandex.Metrika */}
-        <Script
-          id="yandex-metrika"
-          strategy="afterInteractive"
-        >
-          {`
-            (function(m,e,t,r,i,k,a){
-              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) { return; }
-              }
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106295117', 'ym');
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(m,e,t,r,i,k,a){
+  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {
+    if (document.scripts[j].src === r) { return; }
+  }
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+  k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106295117', 'ym');
 
-            ym(106295117, 'init', {
-              ssr:true,
-              webvisor:true,
-              clickmap:true,
-              accurateTrackBounce:true,
-              trackLinks:true,
-              ecommerce:"dataLayer"
-            });
-          `}
-        </Script>
-
+ym(106295117, 'init', {
+  ssr:true,
+  webvisor:true,
+  clickmap:true,
+  accurateTrackBounce:true,
+  trackLinks:true,
+  ecommerce:"dataLayer"
+});
+            `,
+          }}
+        />
         <noscript>
           <div>
             <img
