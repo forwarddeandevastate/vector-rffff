ec2cde5c8c3abf8bd802c4f5bf3226e6c201a7da
fdnfgn0934 <morx.max@gmail.com>
2026-03-08 19:44:57 +0300

    fix build routes

--- app/route/[city]/[to]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { buildRoutePageMetadata, createRoutePage } from "@/lib/route-page";
-
-export const revalidate = 86400;
-export const dynamicParams = true;
-
-export const generateMetadata = buildRoutePageMetadata("route");
-
-export default createRoutePage("route");
